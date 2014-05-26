62ae49d52b6a0ebea1680c2f03281a2d6a13b70c
nschank <nicolas.schank@gmail.com>
2014-05-26 00:09:11 -0400

    Comments and updating types (float->double, Vec->Dimensional)

--- src/nschank/engn/shape/Drawable.java
+++ src/nschank/engn/shape/Drawable.java
@@ -1,6 +1,6 @@
 package nschank.engn.shape;
 
-import cs195n.Vec2f;
+import nschank.collect.dim.Dimensional;
 import nschank.util.Interval;
 
 import java.awt.Color;
@@ -8,26 +8,82 @@ import java.awt.Graphics2D;
 
 
 /**
+ * Created by Nicolas Schank for package nschank.engn.shape
+ * Created on 15 Sep 2013
+ * Last modified on 25 May 2014
+ *
  * An object that can be drawn onto a Graphics2D object. The more general interface that OmegaShapes follow.
  *
- * @author Nicolas Schank
- * @version 2013 09 15
- * @since 2013 09 15 2:40 PM
+ * Central interface of nschank.engn.shape
+ *
+ * @author nschank, Brown University
+ * @version 3.0
  */
 public interface Drawable
 {
+	/**
+	 * A Shape with no size, which does not draw, is black, and is located at the origin.
+	 */
 	public static final Drawable NOTHING = NoShape.NO_SHAPE;
 
+	/**
+	 * Draws this shape on the Graphics object. Should use the pixel position and size of the shape to figure out where
+	 * to draw.
+	 *
+	 * @param g
+	 * 		The Graphics object this shape must be drawn upon.
+	 */
 	public void draw(Graphics2D g);
-	public Vec2f getCenterPosition();
-	public void setCenterPosition(Vec2f centerPosition);
-	public float getHeight();
-	public void setHeight(float h);
-	public float getWidth();
-	public void setWidth(float w);
+
+	/**
+	 * @return The center position of this Drawable object, in a two-dimensional point.
+	 */
+	public Dimensional getCenterPosition();
+
+	/**
+	 * @param centerPosition
+	 * 		The next center position of this Drawable object, in a two-dimensional point.
+	 */
+	public void setCenterPosition(Dimensional centerPosition);
+
+	/**
+	 * @return The height of this object. Depending on time, may be in pixels or game units.
+	 */
+	public double getHeight();
+
+	/**
+	 * @param h
+	 * 		The new height of this object
+	 */
+	public void setHeight(double h);
+	/**
+	 * @return The width of this object. Depending on time, may be in pixels or game units.
+	 */
+	public double getWidth();
+	/**
+	 * @param w
+	 * 		The new width of this object
+	 */
+	public void setWidth(double w);
+	/**
+	 * @return The Color of this object
+	 */
 	public Color getColor();
+	/**
+	 *
+	 * @param c The new Color of this object
+	 */
 	public void setColor(Color c);
 
+	/**
+	 *
+	 * @return The interval this object intersects on the x-axis
+	 */
 	public Interval xInterval();
+
+	/**
+	 *
+	 * @return The interval this object intersects on the y-axis
+	 */
 	public Interval yInterval();
 }
