21096269a157de360ef1f0e42d6130e21340b3d9
nschank <nicolas.schank@gmail.com>
2014-05-27 23:20:03 -0400

    Updates to most of Circle, other than things contained in PCollidable

--- src/nschank/engn/shape/collide/Circle.java
+++ src/nschank/engn/shape/collide/Circle.java
@@ -4,6 +4,9 @@ import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Optional;
 import cs195n.Vec2f;
+import nschank.collect.dim.Dimensional;
+import nschank.collect.dim.Dimensionals;
+import nschank.collect.dim.Vector;
 import nschank.collect.tuple.Pair;
 import nschank.engn.shape.fxn.PointProjector;
 import nschank.util.Interval;
@@ -27,131 +30,220 @@ import java.util.List;
  */
 public class Circle extends PCollidable
 {
-	private Color dotColor;
-	private Vec2f dotPosition;
-	private float rotation = 0.0f;
+	private double rotation = 0.0f;
 
+	/**
+	 *
+	 */
 	private Circle()
 	{
 		super();
-		this.dotColor = new Color(200, 200, 200);
-		this.dotPosition = new Vec2f(0, 0);
 	}
 
-	public Circle(Vec2f location, float radius, Color c)
+	/**
+	 * @param location
+	 * @param radius
+	 * @param c
+	 */
+	public Circle(Dimensional location, double radius, Color c)
 	{
-		super(location, radius * 2f, radius * 2f, c);
-		this.setColor(c);
-		this.dotPosition = location.plus(new Vec2f(radius / 2f, 0));
+		super(location, radius * 2d, radius * 2d, c);
 	}
 
+	/**
+	 *
+	 * @param other
+	 * 		Another object which may be colliding with this one.
+	 *
+	 * @return
+	 */
 	@Override
 	public Optional<Collision> collisionWith(Collidable other)
 	{
 		return inverseOf(other.collisionWithCircle(this));
 	}
 
+	/**
+	 *
+	 * @param other
+	 * 		An AAB that may be colliding with this object
+	 *
+	 * @return
+	 */
 	@Override
 	public Optional<Collision> collisionWithAAB(AAB other)
 	{
-		float clampedX = Math.max(Math.min(this.getCenterPosition().x, other.getCenterPosition().x + (other.getWidth() / 2)), other.getCenterPosition().x - (other.getWidth() / 2));
-		float clampedY = Math.max(Math.min(this.getCenterPosition().y, other.getCenterPosition().y + (other.getHeight() / 2)), other.getCenterPosition().y - (other.getHeight() / 2));
-		if(this.getCenterPosition().minus(clampedX, clampedY).mag2() < (this.getRadius() * this.getRadius()))
+		double clampedX = Math.max(Math.min(this.getCenterPosition().getCoordinate(0), other.getCenterPosition().getCoordinate(0) + (other.getWidth() / 2)), other.getCenterPosition().getCoordinate(0) - (other.getWidth() / 2));
+		double clampedY = Math.max(Math.min(this.getCenterPosition().getCoordinate(1), other.getCenterPosition().getCoordinate(1) + (other.getHeight() / 2)), other.getCenterPosition().getCoordinate(1) - (other.getHeight() / 2));
+		if(new Vector(clampedX, clampedY).minus(this.getCenterPosition()).mag2() < (this.getRadius() * this.getRadius()))
 		{
-			Collision collision = new DefaultCollision(new Vec2f(clampedX, clampedY), this.mtvFromAAB(other));
+			Collision collision = new DefaultCollision(new nschank.collect.dim.Point(clampedX, clampedY), this.mtvFromAAB(other));
 			return Optional.of(collision);
 		}
 		return Optional.absent();
 	}
 
+	/**
+	 *
+	 * @param other
+	 * 		An Circle that may be colliding with this object
+	 *
+	 * @return
+	 */
 	@Override
 	public Optional<Collision> collisionWithCircle(Circle other)
 	{
-		Vec2f apart = other.getCenterPosition().minus(this.getCenterPosition());
+		Vector apart = new Vector(other.getCenterPosition()).minus(this.getCenterPosition());
 		if(apart.mag2() < ((this.getRadius() + other.getRadius()) * (this.getRadius() + other.getRadius())))
 		{
-			float mag = apart.mag();
-			Vec2f napart = apart.sdiv(mag);
-			Collision collision = new DefaultCollision(this.getCenterPosition().plus(napart.smult(this.getRadius())), napart.smult((mag - (other.getRadius() + this.getRadius()))));
+			double mag = apart.mag();
+			Vector napart = apart.normalized();
+			Collision collision = new DefaultCollision(napart.smult(this.getRadius()).plus(this.getCenterPosition()), napart.smult((mag - (other.getRadius() + this.getRadius()))));
+			//todo check if the above should be halved?
 			return Optional.of(collision);
 		}
 		return Optional.absent();
 	}
 
+	/**
+	 *
+	 * @param other
+	 * 		An Point that may be colliding with this object
+	 *
+	 * @return
+	 */
 	@Override
 	public Optional<Collision> collisionWithPoint(Point other)
 	{
 		return this.collisionWithCircle(other);
 	}
 
+	/**
+	 *
+	 * @param other
+	 * 		An Polygon that may be colliding with this object
+	 *
+	 * @return
+	 */
 	@Override
 	public Optional<Collision> collisionWithPolygon(Polygon other)
 	{
 		return inverseOf(other.collisionWithCircle(this));
 	}
 
-	private boolean contains(Vec2f other)
+	/**
+	 * @param other
+	 *
+	 * @return
+	 */
+	public boolean contains(Dimensional other)
 	{
-		float sqdist = ((this.getCenterPosition().x - other.x) * (this.getCenterPosition().x - other.x)) + ((this.getCenterPosition().y - other.y) * (this.getCenterPosition().y - other.y));
+		double myX = this.getCenterPosition().getCoordinate(0);
+		double myY = this.getCenterPosition().getCoordinate(1);
+
+		double otherX = other.getCoordinate(0);
+		double otherY = other.getCoordinate(1);
+
+		double diffX = myX - otherX;
+		double diffY = myY - otherY;
+
+		double sqdist = diffX * diffX + diffY*diffY;
+
 		return sqdist < (this.getRadius() * this.getRadius());
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public Collidable copy()
 	{
 		return new Circle(this.getCenterPosition(), this.getRadius(), this.getColor());
 	}
 
+	/**
+	 *
+	 * @param r
+	 * 		A Ray in the same x-y coordinate plane as this object, which may be pointed to this object
+	 *
+	 * @return
+	 */
 	@Override
-	public Optional<Float> distanceAlong(Ray r)
+	public Optional<Double> distanceAlong(Ray r)
 	{
-		Vec2f relativeCenter = this.getCenterPosition().minus(r.getStartLocation());
-		float projection = relativeCenter.dot(r.getDirection());
-		Vec2f projectionPoint = r.getAtDistance(projection);
-		if(!(this.collisionWithPoint(new Point(projectionPoint)).isPresent() || ((projection < 0) && !this.collisionWithPoint(new Point(r.getStartLocation())).isPresent())))
+		Vector relativeCenter = new Vector(this.getCenterPosition()).minus(r.getStartLocation());
+		double projection = relativeCenter.dotProduct(r.getDirection());
+		Dimensional projectionPoint = r.getAtDistance(projection);
+		if(!(this.contains(projectionPoint) || ((projection < 0) && !this.contains(r.getStartLocation()))))
 			return Optional.absent();
 
-		float x2 = this.getCenterPosition().minus(projectionPoint).mag2();
-		float r2 = this.getRadius() * this.getRadius();
+		double x2 = Dimensionals.sqdistance(this.getCenterPosition(), projectionPoint);
+		double r2 = this.getRadius() * this.getRadius();
 
-		if(this.collisionWithPoint(new Point(r.getStartLocation())).isPresent())
+		if(this.contains(r.getStartLocation()))
 			return Optional.of(projection + (float) Math.sqrt(r2 - x2));
 		else return Optional.of(projection - (float) Math.sqrt(r2 - x2));
 	}
 
+	/**
+	 *
+	 * @param g
+	 */
 	@Override
 	public void draw(Graphics2D g)
 	{
 		g.setColor(this.getColor());
-		g.fillOval((int) (this.getCenterPosition().x - this.getRadius()), (int) (this.getCenterPosition().y - this.getRadius()), (int) this.getWidth(), (int) this.getHeight());
-		g.setColor(this.dotColor);
-		g.drawLine((int) this.dotPosition.x, (int) this.dotPosition.y, (int) this.dotPosition.x, (int) this.dotPosition.y);
+		g.fillOval((int) (this.getCenterPosition().getCoordinate(0) - this.getRadius()), (int) (this.getCenterPosition().getCoordinate(1) - this.getRadius()), (int) this.getWidth(), (int) this.getHeight());
 	}
 
-	public float getRadius()
+	/**
+	 * @return
+	 */
+	public double getRadius()
 	{
-		return this.getHeight() / 2.0f;
+		return this.getHeight() / 2.0;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
-	public float getRotation()
+	public double getRotation()
 	{
 		return this.rotation;
 	}
 
+	/**
+	 *
+	 * @param theta
+	 */
 	@Override
-	public void setRotation(float f)
+	public void setRotation(double theta)
 	{
-		this.rotation = f;
+		this.rotation = theta;
 		this.positionDot();
 	}
 
+	/**
+	 *
+	 * @param mtv
+	 * @param other
+	 * @return
+	 */
 	@Override
 	Optional<Collision> minimumCollisionFrom(Pair<Vec2f, Float> mtv, PCollidable other)
 	{
 		return inverseOf(other.minimumCollisionFromCircle(Pair.tuple(mtv.getA(), -1 * mtv.getB()), this));
 	}
 
+	/**
+	 * TODO
+	 * @param mtv
+	 * @param other
+	 * @return
+	 */
 	@Override
 	Optional<Collision> minimumCollisionFromCircle(Pair<Vec2f, Float> mtv, Circle other)
 	{
@@ -166,56 +258,85 @@ public class Circle extends PCollidable
 		return Optional.absent();
 	}
 
+	/**
+	 *
+	 * @param mtv
+	 * @param other
+	 * @return
+	 */
 	@Override
 	Optional<Collision> minimumCollisionFromPolygon(Pair<Vec2f, Float> mtv, Polygon other)
 	{
 		return inverseOf(other.minimumCollisionFromCircle(Pair.tuple(mtv.getA(), -1 * mtv.getB()), this));
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
-	public float momentOfInertia()
+	public double momentOfInertia()
 	{
-		return (this.getRadius() * this.getRadius()) / 2.0f;
+		return (this.getRadius() * this.getRadius()) / 2.0;
 	}
 
-	private Vec2f mtvFromAAB(AAB other)
+	/**
+	 * @param other
+	 *
+	 * @return
+	 */
+	private Vector mtvFromAAB(AAB other)
 	{
-		if(new Point(this.getCenterPosition()).collisionWithAAB(other).isPresent()) //Circle is inside AAB
+		if(other.contains(this.getCenterPosition())) //Circle is inside AAB
 		{
-			float xDiff = Math.abs(other.getCenterPosition().x - this.getCenterPosition().x);
-			float yDiff = Math.abs(other.getCenterPosition().y - this.getCenterPosition().y);
+			double xDiff = Math.abs(other.getCenterPosition().getCoordinate(0) - this.getCenterPosition().getCoordinate(0));
+			double yDiff = Math.abs(other.getCenterPosition().getCoordinate(1) - this.getCenterPosition().getCoordinate(1));
 
-			float needXDiff = this.getRadius() + (other.getWidth() / 2f);
-			float needYDiff = this.getRadius() + (other.getHeight() / 2f);
+			double needXDiff = this.getRadius() + (other.getWidth() / 2.0);
+			double needYDiff = this.getRadius() + (other.getHeight() / 2.0);
 
 			if(Math.abs(xDiff - needXDiff) < Math.abs(yDiff - needYDiff))
 			{
-				boolean negate = other.getCenterPosition().x > this.getCenterPosition().x;
-				return new Vec2f((negate ? -1f : 1f) * (needXDiff - xDiff), 0);
+				boolean negate = other.getCenterPosition().getCoordinate(0) > this.getCenterPosition().getCoordinate(0);
+				return new Vector((negate ? -1f : 1f) * (needXDiff - xDiff), 0);
 			} else
 			{
-				boolean negate = other.getCenterPosition().y > this.getCenterPosition().y;
-				return new Vec2f(0, (negate ? -1f : 1f) * (needYDiff - yDiff));
+				boolean negate = other.getCenterPosition().getCoordinate(1) > this.getCenterPosition().getCoordinate(1);
+				return new Vector(0, (negate ? -1f : 1f) * (needYDiff - yDiff));
 			}
 		} else
 		{
-			return this.mtvFromCircle(new Point(Math.min(other.getCenterPosition().x + (other.getWidth() / 2), Math.max(other.getCenterPosition().x - (other.getWidth() / 2), this.getCenterPosition().x)), Math.min(other.getCenterPosition().y + (other.getHeight() / 2), Math.max(other.getCenterPosition().y - (other.getHeight() / 2), this.getCenterPosition().y))));
+			double minXa = other.getCenterPosition().getCoordinate(0) + (other.getWidth() / 2d);
+			double minXb = Math.max(other.getCenterPosition().getCoordinate(0) - (other.getWidth() / 2d), this.getCenterPosition().getCoordinate(0));
+			double x = Math.min(minXa, minXb);
+
+			double minYa = other.getCenterPosition().getCoordinate(1) + (other.getHeight() / 2d);
+			double minYb = Math.max(other.getCenterPosition().getCoordinate(1) - (other.getHeight() / 2d), this.getCenterPosition().getCoordinate(1));
+			double y = Math.min(minYa, minYb);
+
+			return this.mtvFromCircle(new Point(x, y));
 		}
 	}
 
-	Vec2f mtvFromCircle(Circle other)
+	/**
+	 * @param other
+	 *
+	 * @return
+	 */
+	Vector mtvFromCircle(Circle other)
 	{
-		Vec2f toCenter = other.getCenterPosition().minus(this.getCenterPosition());
-		float dist = toCenter.mag();
-		float newDist = (other.getRadius() + this.getRadius()) - dist;
+		Vector toCenter = new Vector(other.getCenterPosition()).minus(this.getCenterPosition());
+		double dist = toCenter.mag();
+		double newDist = (other.getRadius() + this.getRadius()) - dist;
 		return toCenter.normalized().smult(-newDist);
 	}
 
-	private void positionDot()
-	{
-		this.dotPosition = this.getCenterPosition().plus(Vec2f.fromPolar(this.rotation, this.getRadius() / 2f));
-	}
-
+	/**
+	 * TODO
+	 * @param axis
+	 * @param projector
+	 * @return
+	 */
 	@Override
 	Interval projectionOnto(Vec2f axis, Function<Vec2f, Float> projector)
 	{
@@ -230,44 +351,40 @@ public class Circle extends PCollidable
 		return theirIt;
 	}
 
+	/**
+	 * @param theta
+	 */
 	@Override
-	public void rotate(float f)
+	public void rotate(double theta)
 	{
-		this.rotation = (float) ((this.rotation + f) % (2 * Math.PI));
-		this.positionDot();
+		this.rotation = ((this.rotation + theta) % (2 * Math.PI));
 	}
 
+	/**
+	 * @param height
+	 */
 	@Override
-	public void setCenterPosition(Vec2f position)
-	{
-		super.setCenterPosition(position);
-		positionDot();
-	}
-
-	@Override
-	public void setColor(Color c)
-	{
-		if((c.getBlue() + c.getGreen() + c.getRed()) < 50) this.dotColor = c.brighter();
-		else this.dotColor = c.darker();
-		super.setColor(c);
-	}
-
-	@Override
-	public void setHeight(float height)
+	public void setHeight(double height)
 	{
 		super.setHeight(height);
 		super.setWidth(height);
-		this.positionDot();
 	}
 
+	/**
+	 *
+	 * @param width
+	 */
 	@Override
-	public void setWidth(float width)
+	public void setWidth(double width)
 	{
 		super.setHeight(width);
 		super.setWidth(width);
-		this.positionDot();
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
 	public String toString()
 	{
