60e88ffae1b437100183d49a698f021869b6e350
nschank <nicolas.schank@gmail.com>
2014-05-29 14:51:57 -0400

    Collidables.contained(Col,List<Dim>)

--- src/nschank/engn/shape/collide/Collidables.java
+++ src/nschank/engn/shape/collide/Collidables.java
@@ -1,17 +1,21 @@
 package nschank.engn.shape.collide;
 
 import com.google.common.base.Optional;
+import nschank.collect.dim.Dimensional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by Nicolas Schank for package nschank.engn.shape.collide;
  * Created on 28 May 2014
- * Last updated on 28 May 2014
+ * Last updated on 29 May 2014
  *
  * A Utility class meant to make working with Collidables and Collisions easier.
  *
  * @author nschank, Brown University
- * @version 1.1
+ * @version 1.2
  */
 public final class Collidables
 {
@@ -44,5 +48,23 @@ public final class Collidables
 	public static void main(String[] args)
 	{
 
+	}
+
+	/**
+	 * Returns all of the given points that are within the object.
+	 *
+	 * @param object
+	 * 		The Collidable which may have some of these points
+	 * @param dims
+	 * 		A collection of points
+	 *
+	 * @return All points {@code dims} that are within the {@code object}
+	 */
+	public static List<Dimensional> contained(final Collidable object, final Iterable<Dimensional> dims)
+	{
+		List<Dimensional> allContained = new ArrayList<>();
+		for(Dimensional dim : dims)
+			if(object.contains(dim)) allContained.add(dim);
+		return allContained;
 	}
 }
