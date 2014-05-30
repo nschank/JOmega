b98f2819b79c79a16c1f2c8fbd1629dd83c21cc5
nschank <nicolas.schank@gmail.com>
2014-05-29 20:35:37 -0400

    AbstractEntity, updated except for Outputs

--- src/nschank/engn/play/AbstractEntity.java
+++ src/nschank/engn/play/AbstractEntity.java
@@ -1,9 +1,9 @@
 package nschank.engn.play;
 
-import com.google.common.base.Function;
 import nschank.collect.tuple.Pair;
 import nschank.engn.play.io.Input;
 import nschank.engn.play.io.InputFailureException;
+import nschank.engn.play.io.Inputs;
 import nschank.engn.play.io.Output;
 import nschank.engn.play.io.eval.Constant;
 import nschank.engn.play.io.eval.Evaluator;
@@ -23,84 +23,64 @@ import java.util.Map;
  * Last updated on 29 May 2014
  *
  * @author nschank, Brown University
- * @version 4.8
+ * @version 5.1
  */
 public abstract class AbstractEntity extends AbstractDrawable implements Entity
 {
-	private final Map<String, Input> inputs;
-	private final Map<String, Output> outputs;
-	private final Map<String, Object> properties;
+	protected final Map<String, Input> inputs;
+	protected final Map<String, Output> outputs;
+	protected final Map<String, Object> properties;
 
-	public AbstractEntity()
+	/**
+	 * Creates an {@code Entity} that exists within the given {@code Universe}
+	 *
+	 * @param owner
+	 * 		The {@code Universe} where this Entity exists
+	 */
+	protected AbstractEntity(Universe owner)
 	{
 		super();
 		this.properties = new HashMap<>();
 		this.outputs = new HashMap<>();
 		this.inputs = new HashMap<>();
-		this.inputs.put("setProperty", new Input()
-		{
-			@Override
-			public void run(final Map<String, Evaluator> args)
-			{
-				AbstractEntity.this.putProperties(NMaps.mapOverValues(args, new Function<Evaluator, Object>()
-				{
-					@Override
-					public Object apply(Evaluator evaluator)
-					{
-						return evaluator.eval(args, AbstractEntity.this);
-					}
-				}));
-			}
-		});
-		this.inputs.put("removeProperty", new Input()
-		{
-			@Override
-			public void run(final Map<String, Evaluator> args)
-			{
-				String ofName = args.get("property").eval(args, AbstractEntity.this).toString();
-				AbstractEntity.this.removeProperty(ofName);
-			}
-		});
-		this.inputs.put("remove", new Input()
-		{
-			@Override
-			public void run(Map<String, Evaluator> args)
-			{
-				if(AbstractEntity.this.hasProperty("universe"))
-					((Universe) AbstractEntity.this.getProperty("universe")).removeEntity(AbstractEntity.this);
-				AbstractEntity.this.getOutput("onRemove").run();
-			}
-		});
 
-		this.inputs.put("runOutput", new Input()
-		{
-			@Override
-			public void run(Map<String, Evaluator> args)
-			{
-				String target = args.get("target").eval(args, AbstractEntity.this).toString();
-				args.remove("target");
-				AbstractEntity.this.getOutput(target).run(args);
-			}
-		});
+		//Default inputs, as per Entity
+		this.inputs.put("!setProperty", Inputs.setProperty(this));
+		this.inputs.put("!removeProperty", Inputs.removeProperty(this));
+		this.inputs.put("!doRemove", Inputs.doRemove(this));
+		this.inputs.put("!runOutput", Inputs.runOutput(this));
+		this.inputs.put("!errorCheckPrint", Inputs.errorCheckPrint(this));
 
-		this.inputs.put("errorCheckPrint", new Input()
-		{
-			@Override
-			public void run(Map<String, Evaluator> args)
-			{
-				for(String in : args.keySet())
-					System.out.println(in + " -> " + args.get(in).eval(args, AbstractEntity.this));
-			}
-		});
+		//Default properties, as per Entity
+		this.properties.put("!self", this);
+		this.properties.put("!universe", owner);
 	}
 
-	public AbstractEntity(Map<String, Object> properties)
+	/**
+	 * Creates an {@code Entity} which exists in the given {@code Universe} and with the given {@code properties}
+	 *
+	 * @param owner
+	 * 		The {@code Universe} where this Entity exists
+	 * @param properties
+	 * 		Any properties this {@code Entity} should have
+	 */
+	protected AbstractEntity(Universe owner, Map<String, Object> properties)
 	{
-		this();
+		this(owner);
+		this.properties.remove("!self");
+		this.properties.remove("!universe");
 		for(String t : properties.keySet())
 			this.properties.put(t, properties.get(t));
 	}
 
+	/**
+	 * Causes an input to be performed, if it has been registered with the object and the "enabled" argument is not false.
+	 * Otherwise, does nothing.
+	 * @param inputType
+	 * 		The name. Often starts with "do" and, if it does, an output will be fired.
+	 * @param arguments
+	 * 		A mapping of arguments from argument name to that value
+	 */
 	@Override
 	public void doInput(String inputType, Map<String, Evaluator> arguments)
 	{
@@ -118,64 +98,98 @@ public abstract class AbstractEntity extends AbstractDrawable implements Entity
 		}
 		if("do".equals(inputType.substring(0, 2)))
 		{
-			this.getOutput("on" + inputType.substring(2)).run(arguments);
+			this.runOutput("on" + inputType.substring(2), arguments);
 		}
 	}
 
+	/**
+	 * Calls the !onDraw Output, with no arguments. Any overriding methods should call {@code super.draw(g)}, or
+	 * (barring that) call the onDraw Output itself.
+	 * @param g
+	 * 		The {@code Graphics2D} object on which this {@code Entity} should be drawn
+	 */
 	@Override
 	public void draw(Graphics2D g)
 	{
-		this.getOutput("onDraw").run();
+		this.runOutput("!onDraw", new HashMap<String, Evaluator>());
 	}
 
-	@Override
-	public Output getOutput(String activatingEvent)
-	{
-		if(this.outputs.containsKey(activatingEvent)) return this.outputs.get(activatingEvent);
-
-		Output ret = new Output();
-		ret.putArgument("!caller", new Constant(this));
-		this.outputs.put(activatingEvent, ret);
-		return ret;
-	}
-
+	/**
+	 * @return All properties of this Entity, in a mapping of name to value
+	 */
 	@Override
 	public Map<String, Object> getProperties()
 	{
 		return Collections.unmodifiableMap(this.properties);
 	}
 
+	/**
+	 * @param ofName
+	 *		The name of a property
+	 * @return The value of that property
+	 */
 	@Override
 	public Object getProperty(String ofName)
 	{
 		return this.properties.get(ofName);
 	}
 
+	/**
+	 * @param ofName
+	 *		The name of a possible Input
+	 * @return Whether that {@code Input} is registered on this Entity
+	 */
 	@Override
 	public boolean hasInput(String ofName)
 	{
 		return this.inputs.containsKey(ofName);
 	}
 
+	/**
+	 * @param ofName
+	 *		The name of a possible property
+	 * @return Whether that property has been assigned a value
+	 */
 	@Override
 	public boolean hasProperty(String ofName)
 	{
 		return this.properties.containsKey(ofName);
 	}
 
+	/**
+	 * Calls the !onTick Output, with the single argument "nanosSinceLastTick"
+	 * @param nanosSinceLastTick
+	 * 		Billionths of a second since the last onTick was called
+	 */
 	@Override
 	public void onTick(long nanosSinceLastTick)
 	{
-		this.getOutput("onTick").run(NMaps
+		this.runOutput("!onTick", NMaps
 				.of(Pair.tuple("nanosSinceLastTick", (Evaluator) new Constant(Float.valueOf(nanosSinceLastTick)))));
+		//todo Evaluators.constant()
 	}
 
+	/**
+	 * Registers an input with this {@code Entity}. If that {@code Input} is internal (starts with an exclamation mark),
+	 * does nothing. The empty string is also an invalid name.
+	 * @param ofName
+	 * 		The name of an {@code Input}
+	 * @param reaction
+	 * 		A reaction which should occur, when {@code doInput} is called with name {@code ofName}
+	 */
 	@Override
 	public void putInput(String ofName, Input reaction)
 	{
+		if(ofName.isEmpty() || ofName.charAt(0) == '!') return;
 		this.inputs.put(ofName, reaction);
 	}
 
+	/**
+	 * Puts all properties in the given map into this {@code Entity}'s property list. Ignores any property names starting
+	 * with an exclamation mark, or the empty string.
+	 * @param properties
+	 * 		A mapping from property names to property values
+	 */
 	@Override
 	public void putProperties(Map<String, Object> properties)
 	{
@@ -183,15 +197,29 @@ public abstract class AbstractEntity extends AbstractDrawable implements Entity
 			this.putProperty(input, properties.get(input));
 	}
 
+	/**
+	 * Puts a single property into this {@code Entity}'s property list.
+	 * @param ofName
+	 * 		The name of a property, which cannot start with an exclamation mark, and cannot be the empty string
+	 * @param ofValue
+	 * 		The value this property should take
+	 */
 	@Override
 	public void putProperty(String ofName, Object ofValue)
 	{
+		if(ofName.isEmpty() || ofName.charAt(0) == '!') return;
 		this.properties.put(ofName, ofValue);
 	}
 
+	/**
+	 * Removes a single property from this {@code Entity}'s property list.
+	 * @param ofName
+	 * 		The name of a property, which cannot start with an exclamation mark, and cannot be the empty string
+	 */
 	@Override
 	public void removeProperty(String ofName)
 	{
+		if(ofName.isEmpty() || ofName.charAt(0) == '!') return;
 		this.properties.remove(ofName);
 	}
 }
