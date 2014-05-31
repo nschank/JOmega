06b259da32b9d2148fa97f456a0d1c1838835d4b
nschank <nicolas.schank@gmail.com>
2014-05-30 21:15:23 -0400

    PhysicsEntity 3.2: - Very small part of the comments - Fixed boolean trap, replaced with Enum ReactionType - Rename confusing "onCollision" with "react"

--- src/nschank/engn/play/phys/PhysicsEntity.java
+++ src/nschank/engn/play/phys/PhysicsEntity.java
@@ -14,13 +14,31 @@ import nschank.engn.shape.collide.Ray;
  * Created on 21 Oct 2013
  * Last updated on 30 May 2014
  *
+ * A {@code PhysicsEntity} is one that is expected to interact in a 2D physical world (aka a {@code Universe}) and, as
+ * such, it must know how to interact with forces, impulses, and collisions with other objects.
+ *
  * @author nschank, Brown University
- * @version 3.1
+ * @version 3.2
  */
 public interface PhysicsEntity extends Entity, Drawable
 {
+	/**
+	 * Applies the given 2D {@code force} (as a {@code Vector}) at a particular {@code position} on the object.
+	 *
+	 * @param force
+	 * @param position
+	 */
 	void applyForceAt(Vector force, Dimensional position);
+	/**
+	 *
+	 * @param impulse
+	 * @param position
+	 */
 	void applyImpulseAt(Vector impulse, Dimensional position);
+	/**
+	 *
+	 * @param change
+	 */
 	void applyLocationChange(Vector change);
 	/**
 	 * Stops all motion of any kind. This includes any derivative of position over time.
@@ -56,6 +74,10 @@ public interface PhysicsEntity extends Entity, Drawable
 	 * 		The new angle of this PhysicsEntity, from the x-axis
 	 */
 	void setAngle(double theta);
+	/**
+	 *
+	 * @return
+	 */
 	double getCoefficientOfDynamicFrictionSqrt();
 	/**
 	 * Used by the collision class; since the coefficient of restitution for a collision
@@ -65,6 +87,10 @@ public interface PhysicsEntity extends Entity, Drawable
 	 * @return The square root of the coefficient of restitution
 	 */
 	double getCoefficientOfRestitutionSqrt();
+	/**
+	 *
+	 * @return
+	 */
 	double getCoefficientOfStaticFrictionSqrt();
 	/**
 	 * @return the mass of this PhysicsEntity
@@ -75,8 +101,20 @@ public interface PhysicsEntity extends Entity, Drawable
 	 * 		the new mass of this PhysicsEntity
 	 */
 	void setMass(double newMass);
+	/**
+	 *
+	 * @return
+	 */
 	double getMomentOfInertia();
+	/**
+	 *
+	 * @return
+	 */
 	double getRotationalVelocity();
+	/**
+	 *
+	 * @param f
+	 */
 	void setRotationalVelocity(double f);
 	/**
 	 * Guaranteed to completely contain the PhysicsEntity.
@@ -84,10 +122,21 @@ public interface PhysicsEntity extends Entity, Drawable
 	 * @return The shape of this PhysicsEntity
 	 */
 	Collidable getShape();
+	/**
+	 *
+	 * @return
+	 */
 	Vector getVelocity();
+	/**
+	 *
+	 * @param newVelocity
+	 */
 	void setVelocity(Dimensional newVelocity);
-	//TODO fix boolean trap
-	void onCollide(PhysCollision collision, boolean impulses, boolean friction);
+	/**
+	 * @param collision
+	 * @param reactionType
+	 */
+	void react(PhysCollision collision, ReactionType reactionType);
 	/**
 	 * Adds to the current angle of this PhysicsEntity
 	 *
@@ -96,12 +145,39 @@ public interface PhysicsEntity extends Entity, Drawable
 	 */
 	void rotate(double plusTheta);
 
+	/**
+	 *
+	 */
 	public static interface PhysCollision extends Collidable.Collision
 	{
+		/**
+		 *
+		 * @return
+		 */
 		Vector getImpulse();
+		/**
+		 *
+		 * @return
+		 */
 		PhysicsEntity getOther();
+		/**
+		 *
+		 * @return
+		 */
 		Vector getSlidingFrictionalImpulse();
+		/**
+		 *
+		 * @return
+		 */
 		@Override
 		PhysCollision inverse();
+	}
+
+	/**
+	 *
+	 */
+	public enum ReactionType
+	{
+		OVERLAP_ONLY, IMPULSE_ONLY, FRICTION_ONLY, FRICTION_AND_IMPULSE
 	}
 }
